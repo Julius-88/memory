30cd89049c25597ab809da2ee14df827c2bbe28d
Julius Alamarvdashti <drako886@gmail.com>
2023-01-24 22:46:50 +0000

    Add a reset button and make sure the DOM is fully loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    flippedCards();
+    resetButton();
+})
+
 const cards = document.getElementsByClassName('cards');
 
+/**
+ * Allows you to flip the cards you click on
+ */
 function flippedCards() {
     for (let i = 0; i < cards.length; i++) {
         cards[i].addEventListener('click', function () {
             this.classList.add('flipped');
         })
     }
-
 }
-flippedCards();
+
+/**
+ * Allows you to reset the board by clicking the reset button
+ */
+function resetButton() {
+    let reset = document.getElementById('reset');
+    reset.addEventListener('click', function () {
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove('flipped');
+        }
+    })
+}
