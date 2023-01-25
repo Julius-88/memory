cd3e53c2dc2923de189206b2d62932d55bc5c4e5
Julius Alamarvdashti <drako886@gmail.com>
2023-01-25 05:27:21 +0000

    Add function to shuffle cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     flippedCards();
     resetButton();
+    shuffleCards();
 })
 
 const cards = document.getElementsByClassName('cards');
+
 
 /**
  * Allows you to flip the cards you click on
@@ -22,8 +24,30 @@ function flippedCards() {
 function resetButton() {
     let reset = document.getElementById('reset');
     reset.addEventListener('click', function () {
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].classList.remove('flipped');
-        }
+        setTimeout(function () {
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].classList.remove('flipped');
+            }
+            setTimeout(shuffleCards, 200);
+        }, 300);
     })
+}
+
+
+// In order to create the shuffleCards I followed this video https://www.youtube.com/watch?v=myL4xmtAVtw
+// and I looked at examples on W3schools. https://www.w3schools.com/js/js_htmldom_nodelist.asp https://www.w3schools.com/jsref/met_node_appendchild.asp
+/**
+ * Shuffles the cards so that they are in a random order
+ */
+function shuffleCards() {
+    for (let i = cards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+    let parent = document.getElementById('memory-container');
+    for (let i = 0; i < cards.length; i++) {
+        parent.appendChild(cards[i]);
+    }
 }
